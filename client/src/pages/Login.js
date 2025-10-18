c882f2d18f4d7d9449f98c9a27e48336ee66eef5
natthidathe <natthida.thee@gmail.com>
2025-10-18 22:16:19 +0700

    landing page + route to login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="login-page">
+      <h2>Login Page</h2>
+      <p>Integrate AWS Cognito login later.</p>
+    </div>
+  );
+}
+
+export default Login;
