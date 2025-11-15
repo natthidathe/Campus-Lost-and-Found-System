dd166247129a5ce00474198127bdeb72683e1dce
kawisaraprew <mindkws@gmail.com>
2025-11-15 22:44:21 +0700

    app.js route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import './css/App.css';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
-import Home from "./pages/Home";
+import Home from "./pages/ClientHome";
+import ReportLost from "./pages/reportlost";
+import ItemDetail from "./pages/ItemDetail";
+import Notifications from "./pages/notifications";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import ReportFound from "./pages/admin/reportfound";
+import AdminItems from "./pages/admin/AdminItems";
+
+
+
 
 
 function App() {
@@ -13,6 +22,14 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/report-lost" element={<ReportLost />} />
+        <Route path="/item/:id" element={<ItemDetail />} />
+        <Route path="/notifications" element={<Notifications />} />
+        {/* Admin routes */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/report-found" element={<ReportFound />} />
+        <Route path="/admin/items" element={<AdminItems />} />
+
       </Routes>
     </Router>
   );
