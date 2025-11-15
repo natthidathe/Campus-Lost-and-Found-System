1ea201db8886a91fba4eb8fa97bc4f68ebf19847
danjkp <164929739+danjkpw@users.noreply.github.com>
2025-11-15 23:35:12 +0700

    matchingService

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ import Login from "./pages/Login";
 import Home from "./pages/ClientHome";
 import ReportLost from "./pages/reportlost";
 import ItemDetail from "./pages/ItemDetail";
-import Notifications from "./pages/notifications";
+//import Notifications from "./pages/notifications";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import ReportFound from "./pages/admin/reportfound";
-import AdminItems from "./pages/admin/AdminItems";
+//import AdminItems from "./pages/admin/AdminItems";
 
 
 
@@ -24,11 +24,11 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/report-lost" element={<ReportLost />} />
         <Route path="/item/:id" element={<ItemDetail />} />
-        <Route path="/notifications" element={<Notifications />} />
+        {/* <Route path="/notifications" element={<Notifications />} /> */}
         {/* Admin routes */}
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/report-found" element={<ReportFound />} />
-        <Route path="/admin/items" element={<AdminItems />} />
+        {/* <Route path="/admin/items" element={<AdminItems />} /> */}
 
       </Routes>
     </Router>
